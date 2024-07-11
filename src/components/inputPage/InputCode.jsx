c04f9cb29ff0e4785ee6c5072dd524422a167d71
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-11 17:20:55 -0300

    feat: Context and navigations
    implemented the useContext to manage the user
    code input and call the gptBot and also navigate
    to output section

--- src/components/inputPage/InputCode.jsx
+++ src/components/inputPage/InputCode.jsx
@@ -1,7 +1,22 @@
 import Editor from "@monaco-editor/react";
 import styles from "../codeBoxes/codeBoxes.module.css";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 function InputCode() {
+  const { userHtml, setUserHtml, gen } = useOutletContext();
+  const nav = useNavigate();
+
+  function handleEditorChange(value) {
+    setUserHtml(value);
+  }
+
+  function handleBtn() {
+    if (userHtml === "") return;
+
+    gen();
+    nav("/details");
+  }
+
   return (
     <div className={styles.box}>
       <h2>Input Html</h2>
@@ -10,8 +25,10 @@ function InputCode() {
         height="400px"
         defaultLanguage="html"
         theme="vs-dark"
+        value={userHtml}
+        onChange={handleEditorChange}
       />
-      <button>Improve Html</button>
+      <button onClick={handleBtn}>Improve Html</button>
     </div>
   );
 }
