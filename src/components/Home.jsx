4e97e0f610493a79b2d046fb15cf553c4f50eab6
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-11 17:16:07 -0300

    feat: implemented Ai and react-router context
    created the ai logic and async call to api
    using "gpt-3.5-turbo" and a prompt,
    gpt returns an object with data
    
    also implemented context to useContext On
    react-router childrens

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,56 @@
 import { Outlet } from "react-router-dom";
+import { createOpenAI } from "@ai-sdk/openai";
+import { generateObject } from "ai";
+import { z } from "zod";
+
+import { useState } from "react";
 
 function Home() {
+  const [response, setResponse] = useState({});
+  const [userHtml, setUserHtml] = useState("");
+
+  const prompt = `
+  Mejora el siguiente codigo html teniendo en cuenta las mejores practicas para accesibilidad web y html semantico,
+  ademas haz puntuaciones de 0 a 100 del codigo de antes y del despues mejorado,
+  agrega tambien unas sugerencias de no mas de 100 palabras para mejorar la accesibilidad del codigo y ten en cuenta de incluir cosas que
+  pueden no estarse viendo a simple vista como un event listener click a un div para que el usuario tenga en cuenta.
+  Este es el codigo HTML:
+  ${userHtml}  
+`;
+
+  const contextValues = {
+    userHtml,
+    setUserHtml,
+    gen,
+    response,
+    setResponse,
+  };
+
+  const openai = createOpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+    compatibility: "strict",
+  });
+
+  async function gen() {
+    try {
+      const { object } = await generateObject({
+        model: openai("gpt-3.5-turbo"),
+        schema: z.object({
+          htmlCode: z.string(),
+          scores: z.object({ after: z.number(), before: z.number() }),
+          suggestions: z.array(z.string()),
+        }),
+        prompt: prompt,
+      });
+
+      console.log(object);
+      setResponse(object);
+    } catch (e) {
+      console.error(e);
+      setResponse(`Error Loading ai model: ${e}`);
+    }
+  }
+
   return (
     <>
       <header>
@@ -11,7 +61,7 @@ function Home() {
       </header>
       <main>
         <section>
-          <Outlet />
+          <Outlet context={contextValues} />
         </section>
       </main>
     </>
