aec608d480deb13a4f17d995b14f6bd756449daf
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-15 16:27:52 -0300

    feat: scores increasing values
    now scores progressivly increases the percentage
    value until is equal to the ai score value,
    this mades an score increasing animation

--- src/components/outputPage/scores/Scores.jsx
+++ src/components/outputPage/scores/Scores.jsx
@@ -1,8 +1,24 @@
 import { Circle } from "rc-progress";
 import styles from "../output.module.css";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 function Scores({ scoreAfter, scoreBefore }) {
+  const [before, setBefore] = useState(0);
+  const [after, setAfter] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (before < scoreBefore) {
+        setBefore((score) => score + 1);
+      }
+
+      if (after < scoreAfter) {
+        setAfter((score) => score + 1);
+      }
+    }, 10);
+  }, [after, before, scoreAfter, scoreBefore]);
+
   return (
     <div>
       <h2>Ai scores</h2>
@@ -11,21 +27,17 @@ function Scores({ scoreAfter, scoreBefore }) {
           <div className={styles.circleBox}>
             <Circle
               className={styles.circle}
-              percent={scoreBefore}
+              percent={before}
               strokeWidth={5}
             />
-            <div className={styles.score}>{scoreBefore}</div>
+            <div className={styles.score}>{before}</div>
           </div>
           <p>Before</p>
         </div>
         <div>
           <div className={styles.circleBox}>
-            <Circle
-              className={styles.circle}
-              percent={scoreAfter}
-              strokeWidth={5}
-            />
-            <div className={styles.score}>{scoreAfter}</div>
+            <Circle className={styles.circle} percent={after} strokeWidth={5} />
+            <div className={styles.score}>{after}</div>
           </div>
           <p>After</p>
         </div>
