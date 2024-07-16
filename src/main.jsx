a9db72ced239640182b3864bd523c336b62f2eae
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-16 11:33:22 -0300

    added main styles file

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import "./mainStyles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
