7e782c2b6ed6f67a52ce5033e0c1795841d5d124
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-17 12:46:58 -0300

    refactored AI logic function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,17 @@
 import { Link, Outlet } from "react-router-dom";
 import { createOpenAI } from "@ai-sdk/openai";
 import { generateObject } from "ai";
-import { z } from "zod";
-
 import { useState } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const openai = createOpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  compatibility: "strict",
+});
 
 function Home() {
   const [response, setResponse] = useState({});
   const [userHtml, setUserHtml] = useState("");
-
-  const prompt = `
-  Mejora el siguiente codigo html teniendo en cuenta las mejores practicas para accesibilidad web y html semantico,
-  ademas haz puntuaciones de 0 a 100 del codigo de antes y del despues mejorado,
-  agrega tambien unas sugerencias de no mas de 100 palabras para mejorar la accesibilidad del codigo y ten en cuenta de incluir cosas que
-  pueden no estarse viendo a simple vista como un event listener click a un div para que el usuario tenga en cuenta.
-  Este es el codigo HTML:
-  ${userHtml}  
-`;
 
   const contextValues = {
     userHtml,
@@ -26,28 +21,15 @@ function Home() {
     setResponse,
   };
 
-  const openai = createOpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    compatibility: "strict",
-  });
-
-  async function gen() {
+  async function gen(obj) {
     try {
-      const { object } = await generateObject({
-        model: openai("gpt-3.5-turbo"),
-        schema: z.object({
-          htmlCode: z.string(),
-          scores: z.object({ after: z.number(), before: z.number() }),
-          suggestions: z.array(z.string()),
-        }),
-        prompt: prompt,
-      });
+      const { object } = await generateObject(obj);
 
       console.log(object);
-      setResponse(object);
+      return object;
     } catch (e) {
       console.error(e);
-      setResponse(`Error Loading ai model: ${e}`);
+      return `Error Loading ai model: ${e}`;
     }
   }
 
