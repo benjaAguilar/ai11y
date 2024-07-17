332710f89ad7d6ed8b6c53f9adf0b75e0bae8134
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-17 17:09:26 -0300

    feat: added radixui components to homepage

--- src/components/homepage/Hompage.jsx
+++ src/components/homepage/Hompage.jsx
@@ -1,21 +1,25 @@
-import styles from "./homep.module.css";
+import { Flex, Heading, Button } from "@radix-ui/themes";
+import { btnSurface, homePageFlex } from "../../styleProps";
+import { DoubleArrowRightIcon } from "@radix-ui/react-icons";
 
 function Hompage() {
   return (
-    <div className={styles.container}>
-      <div className={styles.titleBox}>
-        <h1>
-          <span>AI</span>11Y
-        </h1>
-        <p>
-          improve web accesibility with <span>AI</span>
-        </p>
-      </div>
-      <div className={styles.btnBox}>
-        <button>Semantic Html</button>
-        <button>Color contrast</button>
-      </div>
-    </div>
+    <Flex {...homePageFlex} className="home">
+      <Heading as="h1" className="title">
+        <span>AI</span>11Y
+      </Heading>
+      <Heading as="h2">
+        improve web accesibility with <span>AI</span>
+      </Heading>
+      <Flex gap="1rem" wrap="wrap">
+        <Button {...btnSurface}>
+          Semantic Html <DoubleArrowRightIcon />
+        </Button>
+        <Button {...btnSurface}>
+          Color contrast <DoubleArrowRightIcon />
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
