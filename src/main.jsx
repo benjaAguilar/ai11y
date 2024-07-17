e93009f357a1811f8cece7d8a8ac58593881953c
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-17 16:24:32 -0300

    added radix styles to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import "@radix-ui/themes/styles.css";
 import "./mainStyles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
