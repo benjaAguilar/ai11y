5853eeae2e9ecd0b10275129a63c4ff2ba755f90
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-17 12:47:49 -0300

    refactored AI logic

--- src/components/inputPage/InputCode.jsx
+++ src/components/inputPage/InputCode.jsx
@@ -2,10 +2,31 @@ import Editor from "@monaco-editor/react";
 import * as htmlparser2 from "htmlparser2";
 import styles from "./input.module.css";
 import { useNavigate, useOutletContext } from "react-router-dom";
+import { openai } from "../Home";
+import { z } from "zod";
 
 function InputCode() {
-  const { userHtml, setUserHtml, gen } = useOutletContext();
+  const { userHtml, setUserHtml, gen, setResponse } = useOutletContext();
   const nav = useNavigate();
+
+  const prompt = `
+  Mejora el siguiente codigo html teniendo en cuenta las mejores practicas para accesibilidad web y html semantico,
+  ademas haz puntuaciones de 0 a 100 del codigo de antes y del despues mejorado,
+  agrega tambien unas sugerencias de no mas de 100 palabras para mejorar la accesibilidad del codigo y ten en cuenta de incluir cosas que
+  pueden no estarse viendo a simple vista como un event listener click a un div para que el usuario tenga en cuenta.
+  Este es el codigo HTML:
+  ${userHtml}  
+`;
+
+  const aiObject = {
+    model: openai("gpt-3.5-turbo"),
+    schema: z.object({
+      htmlCode: z.string(),
+      scores: z.object({ after: z.number(), before: z.number() }),
+      suggestions: z.array(z.string()),
+    }),
+    prompt: prompt,
+  };
 
   function handleEditorChange(value) {
     setUserHtml(value);
@@ -25,7 +46,7 @@ function InputCode() {
     }
   }
 
-  function handleBtn() {
+  async function handleBtn() {
     const isValidHtml = isHtml();
 
     if (!isValidHtml) {
@@ -33,7 +54,8 @@ function InputCode() {
       return;
     }
 
-    gen();
+    const res = await gen(aiObject);
+    setResponse(res);
     nav("/semantic-html/details");
   }
 
