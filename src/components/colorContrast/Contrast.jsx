1d2f43e192a9496ae2f30d63e3370909a41ca198
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-17 12:48:24 -0300

    feat: AI colors suggestions
    now if you press a btn ai suggests similar colors
    to try

--- src/components/colorContrast/Contrast.jsx
+++ src/components/colorContrast/Contrast.jsx
@@ -1,11 +1,58 @@
 import { useState } from "react";
 import Color from "color";
 import InputHex from "./inputHex/InputHex";
+import { openai } from "../Home";
+import { z } from "zod";
+import { useOutletContext } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 function Contrast() {
   const [bgValue, setBgValue] = useState("#262626");
   const [fgValue, setFgValue] = useState("#DBFF5E");
   const [ratio, setRatio] = useState(calcRatio(bgValue, fgValue).toFixed(2));
+  const [aiSuggestions, setAiSuggestions] = useState({
+    suggestion: "",
+    colorIdeas: [],
+  });
+  const { gen } = useOutletContext();
+
+  const prompt = `
+  Hazme una sugerencia de no mas de 50 palabras de como mejorar el contraste 
+  en los colores ${bgValue} y ${fgValue} para mejorar la accecibilidad web sin 
+  incluir los colores en la sugerencia,
+  si su contrast ratio ya de por si cumple con estandares AA intenta mejorarlo a AAA o
+  si no cumple ningun estandar intenta mejorarlo lo maximo que puedas. Incluye 5 ideas de colores
+  dentro de colorIdeas manteniendo la estructura de array de objetos cada idea con su background y foreground utilizando
+  colores mas similares posibles a ${bgValue} y ${fgValue} en formato hex tal que mejoren o mantengan un buen nivel de contrast ratio. 
+`;
+
+  const aiObject = {
+    model: openai("gpt-3.5-turbo"),
+    schema: z.object({
+      suggestion: z.string(),
+      colorIdeas: z.array(
+        z.object({ backGround: z.string(), foreGround: z.string() })
+      ),
+    }),
+    prompt: prompt,
+  };
+
+  async function handleAiSuggestions() {
+    const res = await gen(aiObject);
+
+    const arr = [];
+    res.colorIdeas.forEach((idea) => arr.push({ ...idea, id: uuidv4() }));
+
+    const obj = { ...res, colorIdeas: arr };
+    setAiSuggestions(obj);
+  }
+
+  function useColorIdea(id) {
+    const colors = aiSuggestions.colorIdeas.filter((color) => color.id === id);
+    setBgValue(colors[0].backGround);
+    setFgValue(colors[0].foreGround);
+    handleInputSet();
+  }
 
   function switchValues() {
     setBgValue(fgValue);
@@ -67,6 +114,27 @@ function Contrast() {
         <p>
           {ratio} {ratio <= 4.5 ? "poor" : ratio >= 7 ? "very good" : "good"}
         </p>
+      </div>
+      <div>
+        <button onClick={handleAiSuggestions}>AI suggestions</button>
+        <div>
+          <h3>color contrasts suggestions</h3>
+          <p>{aiSuggestions.suggestion}</p>
+          <div>
+            {aiSuggestions.colorIdeas.map((colors) => {
+              return (
+                <div key={colors.id} onClick={() => useColorIdea(colors.id)}>
+                  <div>
+                    <p>{colors.backGround}</p>
+                  </div>
+                  <div>
+                    <p>{colors.foreGround}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
       <div style={{ backgroundColor: bgValue }}>
         <h2 style={{ color: fgValue }}>Example box</h2>
