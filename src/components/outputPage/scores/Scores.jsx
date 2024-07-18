c170708f03ce1440b0330c81539ebb1bf2ded9b7
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-18 14:05:09 -0300

    feat: added radix-ui components

--- src/components/outputPage/scores/Scores.jsx
+++ src/components/outputPage/scores/Scores.jsx
@@ -2,6 +2,7 @@ import { Circle } from "rc-progress";
 import styles from "../output.module.css";
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
+import { Flex, Heading } from "@radix-ui/themes";
 
 function Scores({ scoreAfter, scoreBefore }) {
   const [before, setBefore] = useState(0);
@@ -20,10 +21,10 @@ function Scores({ scoreAfter, scoreBefore }) {
   }, [after, before, scoreAfter, scoreBefore]);
 
   return (
-    <div>
-      <h2>Ai scores</h2>
+    <Flex direction="column" gap="1rem">
+      <Heading as="h2">Ai scores</Heading>
       <div className={styles.circlesBox}>
-        <div>
+        <Flex direction="column" justify="center" align="center" gap="1rem">
           <div className={styles.circleBox}>
             <Circle
               className={styles.circle}
@@ -32,17 +33,21 @@ function Scores({ scoreAfter, scoreBefore }) {
             />
             <div className={styles.score}>{before}</div>
           </div>
-          <p>Before</p>
-        </div>
-        <div>
+          <Heading as="h5" size="4">
+            Before
+          </Heading>
+        </Flex>
+        <Flex direction="column" justify="center" align="center" gap="1rem">
           <div className={styles.circleBox}>
             <Circle className={styles.circle} percent={after} strokeWidth={5} />
             <div className={styles.score}>{after}</div>
           </div>
-          <p>After</p>
-        </div>
+          <Heading as="h5" size="4">
+            After
+          </Heading>
+        </Flex>
       </div>
-    </div>
+    </Flex>
   );
 }
 
