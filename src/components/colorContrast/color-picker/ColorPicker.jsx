073530b774a96c611d2b7024816617ef976f29a2
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-19 14:54:30 -0300

    feat: added radix ui components

--- src/components/colorContrast/color-picker/ColorPicker.jsx
+++ src/components/colorContrast/color-picker/ColorPicker.jsx
@@ -1,14 +1,29 @@
 import * as Popover from "@radix-ui/react-popover";
 import { SketchPicker } from "react-color";
 import PropTypes from "prop-types";
+import { Button, Card, IconButton, Flex } from "@radix-ui/themes";
+import { BlendingModeIcon } from "@radix-ui/react-icons";
+import { btnSurfaceS } from "../../../styleProps";
 
 function ColorPicker({ color, handlePickerChange }) {
   return (
     <Popover.Root>
-      <Popover.Trigger>color</Popover.Trigger>
-      <Popover.Content>
-        <SketchPicker color={color} onChange={handlePickerChange} />
-        <Popover.Close>done</Popover.Close>
+      <Popover.Trigger className="resetPopover">
+        <IconButton variant="surface" size="3">
+          <BlendingModeIcon />
+        </IconButton>
+      </Popover.Trigger>
+      <Popover.Content className="popover">
+        <Card>
+          <Flex direction="column" gap="1rem">
+            <SketchPicker color={color} onChange={handlePickerChange} />
+            <Popover.Close className="resetPopover" style={{ width: "100%" }}>
+              <Button {...btnSurfaceS} style={{ width: "100%" }}>
+                Done
+              </Button>
+            </Popover.Close>
+          </Flex>
+        </Card>
       </Popover.Content>
     </Popover.Root>
   );
