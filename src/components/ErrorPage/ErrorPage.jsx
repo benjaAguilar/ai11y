5be163a7e1ff323992a3ba3a3bae67128b864366
Benja Aguilar <benjaaguilar02@gmail.com>
2024-07-22 12:42:38 -0300

    feat: added radix ui

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
+import { Theme, Card, Flex, Heading } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
 function ErrorPage() {
   return (
-    <div>
-      <h1>Whoops! 404 page not found</h1>
-      <p>seems that path does not exist</p>
-      <Link to={"/"}>Go back to home</Link>
-    </div>
+    <Theme
+      appearance="dark"
+      panelBackground="translucent"
+      accentColor="iris"
+      radius="large"
+      scaling="100%"
+    >
+      <Flex justify="center" align="center" className="errorPage">
+        <Card>
+          <Flex direction="column" align="center" justify="center" gap="3rem">
+            <Heading as="h1" size="8">
+              Whoops! <span>404</span> page not found
+            </Heading>
+            <Heading as="h2" size="5">
+              Seems that this path does not exist
+            </Heading>
+            <Link to={"/"}>Go back to home</Link>
+          </Flex>
+        </Card>
+      </Flex>
+    </Theme>
   );
 }
 
